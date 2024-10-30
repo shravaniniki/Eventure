47d0eca394fd44a43d92aa04f5c6fb470f0982ab
shravaniniki <shravs0403@gmail.com>
2024-10-30 13:48:14 +0530

    routing

--- Eventure/src/app/app.routes.ts
+++ Eventure/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { OrganizerComponent } from './profile/components/organizer/organizer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '',component: HomeComponent, title:'Home'},
+  {path: 'about',component:AboutComponent, title: 'About'},
+  {path: 'profile', children:[
+    {path: 'organizer', component: OrganizerComponent, title: 'Profile'}
+  ]}
+  
+];
