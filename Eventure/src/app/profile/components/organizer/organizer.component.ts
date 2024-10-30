7fdf2722812213c3cf486244e42740d5546d9e8e
shravaniniki <shravs0403@gmail.com>
2024-10-30 13:41:26 +0530

    components

---
+++ Eventure/src/app/profile/components/organizer/organizer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer',
+  standalone: true,
+  imports: [],
+  templateUrl: './organizer.component.html',
+  styles: ``
+})
+export class OrganizerComponent {
+
+}
