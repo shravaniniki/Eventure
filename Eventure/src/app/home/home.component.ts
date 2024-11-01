7cb33e65ebc22f00208fb1f39b1ef8367806774a
varshini-sriram <VS94@ford.com>
2024-11-01 23:31:08 +0530

    updated home page

--- Eventure/src/app/home/home.component.ts
+++ Eventure/src/app/home/home.component.ts
@@ -7,24 +7,24 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: `./home.component.css`
 })
 export class HomeComponent {
   events: IEvent[] = [];
   router: any;
-  
-  constructor(private eventService: EventService) {
-    
-  }
 
+  constructor(private eventService: EventService) {
+
+  }
   ngOnInit(): void {
     // Connect to the Service using dep injection
     // 1. Send the request to the service
     // 2. Get the response from the service
-
+    
     this.eventService.getEvents().subscribe((response: IEvent[]) => {
+      
       console.log(response);
       this.events = response;
     })
