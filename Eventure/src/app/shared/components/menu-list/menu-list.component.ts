179b727033671abb504d1ee79a42b1bbb71063c7
shravaniniki <shravs0403@gmail.com>
2024-11-01 14:18:54 +0530

    Footer Changes

--- Eventure/src/app/shared/components/menu-list/menu-list.component.ts
+++ Eventure/src/app/shared/components/menu-list/menu-list.component.ts
@@ -15,10 +15,39 @@ import { RouterModule } from '@angular/router';
     color: gray;
     font-weight: bold;
   }
+  
   button{
     margin-left: 10px;
   }
- 
+
+  .nav-menu {
+  position: relative; /* Needed for absolute positioning of the underline */
+  }
+
+  .nav-menu a {
+  position: relative; /* Needed for absolute positioning of the underline */
+  display: block;     /* Makes the link take up the full width of its container */
+  padding-bottom: 5px; /* Add some space for the underline */
+  }
+
+  .nav-menu a::before {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 0;
+  height: 2px;
+  background-color: #f82249; /* Or your desired color */
+  transition: width 0.3s ease; /* Add a smooth transition */
+  }
+
+  .nav-menu a:hover::before {
+  width: 100%; /* Extend the underline to full width on hover */
+  }
+
+.nav-menu a.active::before {
+    width: 100%; /* Extend the underline when the link is active */
+}
   `
 })
 export class MenuListComponent {
@@ -30,5 +59,5 @@ export class MenuListComponent {
 
   ];
 
-  
+
 }
