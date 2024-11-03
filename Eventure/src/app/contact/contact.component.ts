36c6ac633099556d19d7af48e1380c148f12ec17
varshini-sriram <varshinisriram@outlook.com>
2024-11-03 22:26:59 +0530

    updated contact page

--- Eventure/src/app/contact/contact.component.ts
+++ Eventure/src/app/contact/contact.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
@@ -16,23 +16,26 @@ import { Component } from '@angular/core';
     <h3>Contact Information</h3>
     <div class="row">
         <div class="col-md-6">
-            <h4>Email Ids</h4>
+            <h4>Email IDs</h4>
             <ul class="list-group">
                 <li class="list-group-item">Varshini: varshinisriram02&#64;gmail.com</li>
-                <li class="list-group-item">Shravani: shravani&#64;gmail.com </li>
+                <li class="list-group-item">Shravani: shravani&#64;gmail.com</li>
                 <li class="list-group-item">Sharmila: sharmila&#64;gmail.com</li>
-                <li class="list-group-item">Janani: janani&#64;gmail.com </li>
+                <li class="list-group-item">Janani: janani&#64;gmail.com</li>
             </ul>
         </div>
         <div class="col-md-6">
-            <h4>Phone Number</h4>
-            <p class="text-muted">1234567899</p>
-            <p class="text-muted">1234567899</p>
-            <p class="text-muted">1234567899</p>
-            <p class="text-muted">1234567899</p>
+            <h4>Phone Numbers</h4>
+            <ul class="list-group">
+                <li class="list-group-item text-muted">1234567899</li>
+                <li class="list-group-item text-muted">1234567899</li>
+                <li class="list-group-item text-muted">1234567899</li>
+                <li class="list-group-item text-muted">1234567899</li>
+            </ul>
         </div>
     </div>
 </div>
+
 
   `,
   styles: `
