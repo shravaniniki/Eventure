3d5905b7e7581aa5ef1da5a605684c26e708f4b2
shravaniniki <shravs0403@gmail.com>
2024-11-03 10:36:56 +0530

    Backend

---
+++ src/test/java/com/ford/demo/EventureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ford.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
