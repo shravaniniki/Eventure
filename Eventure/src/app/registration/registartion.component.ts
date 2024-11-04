2de2244dd97e3bc346229566435d1c0e2e215ab4
JANANi4098 <jananijanani4098@gmail.com>
2024-11-04 21:00:39 +0530

    Registration page validation added

--- Eventure/src/app/registration/registartion.component.ts
+++ Eventure/src/app/registration/registartion.component.ts
@@ -1,13 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
+import { EventService } from '../events/services/event.service';
 @Component({
   selector: 'app-user-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,RouterModule,CommonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class UserSettingsComponent {
+  registrationForm: FormGroup;
+  isSaved = false;
+  constructor(private eventServiceService: EventService) {
+    this.registrationForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      phone: new FormControl('', [Validators.required, Validators.pattern("[0-9]{10}")]),
+      email: new FormControl('', [Validators.required, Validators.email]),
 
+    });
+  }
+  handleSubmit() {
+   
+//         const eventData = {
+//             ...this.registrationForm.value,
+//             e_date: this.registrationForm.value.date, // Rename for service if necessary
+//             e_mode: this.registrationForm.value.mode  // Rename for service if necessary
+//         };
+//         console.log('Event Data:', eventData); // Check the data before sending
+
+//         this.eventServiceService.addEvent(eventData)
+//             .subscribe((response: any) => {
+//                 console.log(response);
+//                 this.isSaved = true;
+//                 this.registrationForm.reset();//Reset form on success
+//             });  
 }
+}
+
+
